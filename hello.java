1171b795767d8c8a0711c665e9da67bc5e9a03a9
MartinLiuJianCheng <2285830341@qq.com>
2020-03-24 00:47:00 +0800

    hello.java branch1 添加

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+	public static void main(string[] args){
+			
+	system out.println("Hello World");
+
+	
+	system out.println("1");
+	system out.println("2");
+	system out.println("Hello World");
+
+
+
+
+
+
+			}
+}
